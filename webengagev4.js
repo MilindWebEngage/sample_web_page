b06c9c92ce7a0f32d3e13ae778e2b13269c45323
MilindWebEngage <111427433+MilindWebEngage@users.noreply.github.com>
2026-09-03 17:15:41 +0530

    Update and rename webengagev3.js to webengagev4.js

--- webengagev4.js
+++ webengagev4.js
@@ -912,7 +912,7 @@
             streakDate: streakDate,
 
             cycleStartDate: cycleStartDate,
-            user : {{user['custom']}},
+            user : '{{user['custom']}}',
 
             milestone:
               streakCount >= 7
